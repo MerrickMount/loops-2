16dcb339233cd12087a5ba8c083fece1edd98156
Merrick Mount <339474587@GoTVDSB.ca>
2024-04-22 12:06:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace loops_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double testAmount, testScore, testPass;
+            testPass = 0;
+            Console.WriteLine("Welcome to the test checker");
+            Console.WriteLine("Please enter how many test scores are being entered");
+            testAmount = double.Parse(Console.ReadLine());
+            for (double i = testAmount; i <= 0; i--)
+            {
+                Console.WriteLine("please input test score:");
+                testScore = double.Parse(Console.ReadLine());
+                if (testScore <= 70)
+                {
+                    testPass++;
+
+                }
+            }
+            Console.WriteLine($"{testPass} of the tests were above 70%");
+            Console.WriteLine($"that is a pass average of {testPass/testAmount}%");
+        }
+    }
+}
