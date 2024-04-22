a071fe314f81704ec5f2f9f8a7a71f471acce600
Merrick Mount <339474587@GoTVDSB.ca>
2024-04-22 12:33:36 -0400

    DONE

--- Program.cs
+++ Program.cs
@@ -6,23 +6,26 @@ namespace loops_2
     {
         static void Main(string[] args)
         {
-            double testAmount, testScore, testPass;
+            int testAmount, testScore, testPass;
             testPass = 0;
             Console.WriteLine("Welcome to the test checker");
             Console.WriteLine("Please enter how many test scores are being entered");
-            testAmount = double.Parse(Console.ReadLine());
-            for (double i = testAmount; i <= 0; i--)
+            testAmount = int.Parse(Console.ReadLine());
+            for (int i = testAmount; i > 0; i--)
             {
                 Console.WriteLine("please input test score:");
-                testScore = double.Parse(Console.ReadLine());
+                testScore = int.Parse(Console.ReadLine());
                 if (testScore <= 70)
                 {
                     testPass++;
-
+                    Console.WriteLine("Test Entered");
+                }
+                else
+                {
+                    Console.WriteLine("Test Entered");
                 }
             }
             Console.WriteLine($"{testPass} of the tests were above 70%");
-            Console.WriteLine($"that is a pass average of {testPass/testAmount}%");
         }
     }
 }
